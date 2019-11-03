dea66ed713de44480763ec572185557aa664bdbf
Jonah Latham <jonah.latham21@gmail.com>
2019-11-03 13:09:51 -0700

    code

--- src/Pages/CreateNewItem/CreateNewItem.js
+++ src/Pages/CreateNewItem/CreateNewItem.js
@@ -49,26 +49,33 @@ class CreateNewItem extends Component {
             .then((response) => {
                 if (response.data.success) {
                     this.props.dispatch({ type: 'SUBMIT' })
-                    this.props.history.push('/home')
+                    this.props.history.push('/displayedItems')
                 } else {
                     alert(response.data.err)
                 }
             })
     }
+
+    handleClear = () => {
+        this.props.dispatch({ type: 'SUBMIT' })
+        window.location.reload()
+    }
     render() {
         console.log(this.props.item)
         const loopedHolidays = this.state.holidays.map((e) => {
-            return <option key={e.id} value={e.id}>{e.name}</option>
+            return <option className='createNewOptions' key={e.id} value={e.id}>{e.name}</option>
         })
         return (
             <div className='createNewApp'>
-                <input placeholder='Item' onChange={this.handleNewItem} type="text" value={this.props.newItem} />
-                <input placeholder='Image URL' onChange={this.handleImage} type="text" value={this.props.itemImage} />
-                <select onChange={this.handleHoliday}>
-                    <option value="">Select</option>
+                <input className='createNewInputs' placeholder='Item' onChange={this.handleNewItem} type="text" value={this.props.newItem} />
+                <input className='createNewInputs' placeholder='Image URL' onChange={this.handleImage} type="text" value={this.props.itemImage} />
+                <select className='createNewSelect' onChange={this.handleHoliday}>
+                    <option className='createNewOptions' value="">Select</option>
                     {loopedHolidays}
                 </select>
                 <button onClick={this.CreateNewItemSubmit}>Submit</button>
+                <br />
+                <button style={{ width: '50px', margin: 'auto' }} onClick={this.handleClear}>Clear</button>
             </div>
         );
     }
