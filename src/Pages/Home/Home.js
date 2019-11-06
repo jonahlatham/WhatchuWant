490065efc9b71181249e733772f9e1c5deea2b90
Jonah Latham <jonah.latham21@gmail.com>
2019-11-05 19:25:30 -0700

    code

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import axios from 'axios'
 
 class Home extends Component {
-
     render() {
         return (
             <div>
