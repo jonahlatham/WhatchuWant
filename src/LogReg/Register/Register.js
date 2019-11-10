6e3397ee75198baa8067186d8a40514206ec7649
Jonah Latham <jonah.latham21@gmail.com>
2019-11-10 13:46:07 -0500

    code

--- src/LogReg/Register/Register.js
+++ src/LogReg/Register/Register.js
@@ -10,7 +10,7 @@ class Register extends Component {
         last_name: '',
         email: '',
         password: '',
-        dob: ''
+        doubleCheck: ''
     }
 
     handleChange = (event) => {
@@ -20,12 +20,22 @@ class Register extends Component {
     }
 
     handleRegister = () => {
-        const body = {
-            first_name: this.state.first_name,
-            last_name: this.state.last_name,
-            email: this.state.email,
-            password: this.state.password,
-        }
+        if (this.state.password !== this.state.doubleCheck) {
+            alert('Password does not match')
+            this.setState({
+                first_name: '',
+                last_name: '',
+                email: '',
+                password: '',
+                doubleCheck: ''
+            })
+        } else {
+            const body = {
+                first_name: this.state.first_name,
+                last_name: this.state.last_name,
+                email: this.state.email,
+                password: this.state.password,
+            }
             axios.post('/auth/register', body)
                 .then((response) => {
                     if (response.data.success) {
@@ -42,6 +52,7 @@ class Register extends Component {
                         dob: ''
                     })
                 })
+        }
     }
 
     render() {
@@ -51,6 +62,7 @@ class Register extends Component {
                 <input style={{ boxShadow: /^[a-zA-Z]+$/.test(this.state.last_name) ? '0 0 3pt 2pt green' : '0 0 3pt 2pt red' }} className='registerInputs' name='last_name' value={this.state.last_name} onChange={this.handleChange} placeholder='LAST NAME' type="text" />
                 <input style={{ boxShadow: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email) ? '0 0 3pt 2pt green' : '0 0 3pt 2pt red' }} className='registerInputs' name='email' value={this.state.email} onChange={this.handleChange} placeholder='EMAIL' type="text" />
                 <input style={{ boxShadow: /^(?=.*[0-9])(?=.*[!@#$%^&*_+-=:;'()])[a-zA-Z0-9!@#$%^&*_+-=:;'()]{7,15}$/.test(this.state.password) ? '0 0 3pt 2pt green' : '0 0 3pt 2pt red' }} className='registerInputs' name='password' value={this.state.password} onChange={this.handleChange} placeholder='PASSWORD' type="text" />
+                <input style={{ boxShadow: this.state.password === this.state.doubleCheck ? '0 0 3pt 2pt green' : '0 0 3pt 2pt red' }} className='registerInputs' name='doubleCheck' value={this.state.doubleCheck} onChange={this.handleChange} placeholder='RE-ENTER PASSWORD' type="text" />
                 <small>
                     Password must have 7-15 characters with at least one numeric digit and a special character
                 </small>
